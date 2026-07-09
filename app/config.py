f2a7f7a65f3aba5f34620a156743669203188672
Krishankant89 <toxickush90@gmail.com>
2026-07-09 12:49:35 +0530

    Stores the application configuration

--- app/config.py
+++ app/config.py
@@ -1,6 +1,8 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 class Config:
-    SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
+    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
